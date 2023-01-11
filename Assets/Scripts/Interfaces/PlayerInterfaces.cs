5c3575364ff68f77bf001e5687375ebeb0e1ddb5
ZorJinklesin <zorjinklesin@gmail.com>
2023-01-11 17:24:57 +0800

    角色接口文件转移

--- Assets/Scripts/Interfaces/PlayerInterfaces.cs
+++ Assets/Scripts/Interfaces/PlayerInterfaces.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace Interfaces
+public interface PlayerInterfaces
 {
-    public interface PlayerInterfaces
-    {
-        //激活护盾，参数time：持续时间
-        public void ActivateSheild(float time);
+    // Start is called before the first frame update
 
-        //将人物移动到vector位置
-        public void ToPosition(Vector3 vector);
+    ////激活护盾，参数time：持续时间
+    public void ActivateSheild(float time);
 
-        //将人物根据朝向闪现distance个单位
-        public void ActivateFlash(float distance);
-    }
+    //将人物移动到vector位置
+    public void ToPosition(Vector3 vector);
+
+    //将人物根据朝向闪现distance个单位
+    public void ActivateFlash(float distance);
+
+    public bool IsDie();
+    public bool IsHurt();
 }
-
