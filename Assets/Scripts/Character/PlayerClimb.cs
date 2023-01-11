e1619e1a5532cbeb3344c180f795c71e0fc19609
GameIdea <799375868@qq.com>
2023-01-11 15:02:04 +0800

    First Level and some ArtSource

--- Assets/Scripts/Character/PlayerClimb.cs
+++ Assets/Scripts/Character/PlayerClimb.cs
@@ -25,11 +25,11 @@ public class PlayerClimb : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) && canClimb)
         {
-            rb.velocity = new Vector2(rb.velocity.x, climbSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, climbSpeed * playerMovement.inputY);
         }
         if (Input.GetKeyDown(KeyCode.S) && canClimb)
         {
-            rb.velocity = new Vector2(rb.velocity.x, -climbSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, -climbSpeed * playerMovement.inputY);
         }
     }
 
