3fad843473f4034a9c283327c08e95e12e96fc4e
ZorJinklesin <zorjinklesin@gmail.com>
2023-01-12 09:52:26 +0800

    日记本道具

---
+++ Assets/Scripts/Items/DiaryItemObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Items;
+
+public class DiaryItemObject : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("名称")]
+    private string itemName;
+
+    private bool isPicked;
+    private DiaryItem item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.isPicked = false;
+        this.item = new DiaryItem(this.itemName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isPicked)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(this.itemName + "道具触发碰撞");
+        if (Item.IsHolder(collider.gameObject))
+        {
+            this.item.SetHolder(collider.gameObject);
+            collider.gameObject.SendMessage("AddItem", this.item);
+            this.isPicked = true;
+        }
+    }
+
+}
+
+namespace Items
+{
+    public class DiaryItem: Item
+    {
+        private bool isUsing;
+        public DiaryItem(string name)
+        {
+            this.name = name;
+            this.isUsing = false;
+        }
+
+        public override void ItemInvoke()
+        {
+            if (!this.isUsing)
+            {
+                Debug.Log(this.name + ": 查看日记本");
+                //this.holder.SendMessage("ShowDiaryContent");
+            }
+            else
+            {
+                Debug.Log(this.name + ": 收起日记本");
+                //this.holder.SendMessage("HideDiaryContent");
+            }
+
+            this.isUsing = !this.isUsing;
+        }
+    }
+}
