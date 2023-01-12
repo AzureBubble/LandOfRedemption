802c0f1123672955b2da5fdb7c9127f6e2245d66
ZorJinklesin <zorjinklesin@gmail.com>
2023-01-12 18:39:46 +0800

    接口规范

--- Assets/Scripts/Interfaces/PlayerInterfaces.cs
+++ Assets/Scripts/Interfaces/PlayerInterfaces.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Interfaces
-{ 
-public interface PlayerInterfaces
 {
-    ////激活护盾，参数time：持续时间
-    public void ActivateSheild(float time);
+    public interface PlayerInterfaces
+    {
+        ////激活护盾，参数time：持续时间
+        public void ActivateSheild(float time);
 
-    //将人物移动到vector位置
-    public void ToPosition(Vector3 vector);
+        //将人物移动到vector位置
+        public void ToPosition(Vector3 vector);
 
-    //将人物根据朝向闪现distance个单位
-    public void ActivateFlash(float distance);
+        //影子位置
+        public void ShadowPosition(Vector3 vector);
 
-}
+        //将人物根据朝向闪现distance个单位
+        public void ActivateFlash(float distance);
+
+    }
 }
--- Assets/Scripts/Interfaces/UIforItemInterface.cs
+++ Assets/Scripts/Interfaces/UIforItemInterface.cs
@@ -27,16 +27,13 @@ namespace Interfaces
         //输入密码UI， number：0-9的数字字符
         public void InputPassword(char number);
 
+        //输入密码清空
+        public void InputClear();
+
         //道具移除时的UI展示，name：道具名称
         public void RemoveItem(string name);
 
-        //道具加入时的UI展示，name：道具名称
-        public void AddItem(string name);
-
-        //向左切换道具时的UI展示，leftItemName:道具栏左移后的道具名供校准
-        public void SwitchItemLeft(string leftItemName);
-
-        //向右切换道具时的UI展示，rightItemName:道具栏左移后的道具名供校准
-        public void SwitchItemRight(string rightItemName);
+        //展示道具时的UI展示，name：道具名称
+        public void ShowItem(string name);
     }
 }
