fa33ac1b78ee1a4d568aa257ea97bfbf29a154d4
ZorJinklesin <zorjinklesin@gmail.com>
2023-01-12 09:52:50 +0800

    传感门与传感器道具

---
+++ Assets/Scripts/Items/SensorItemObject.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Items;
+
+public class SensorItemObject : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("名称")]
+    private string itemName;
+    [SerializeField]
+    [Tooltip("激活过期时间")]
+    private float expiredTime;
+    //传感器激活的门
+    private GameObject sensorDoor;
+    private bool isConnected;
+    private SensorItem item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.isConnected = false;
+        this.item = new SensorItem(this.itemName, this.expiredTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(this.itemName + ": " + this.sensorDoor.name);
+        if (this.sensorDoor)
+        {
+            this.isConnected = true;
+        }
+        else
+        {
+            this.isConnected = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(this.itemName + "道具触发碰撞");
+        if (this.isConnected && Item.IsHolder(collider.gameObject))
+        {
+            this.item.SetHolder(this.sensorDoor);
+            this.item.ItemInvoke();
+        }
+    }
+
+    public void BuildLink(GameObject obj)
+    {
+        this.sensorDoor = obj;
+    }
+}
+
+namespace Items
+{
+    public class SensorItem: Item
+    {
+        private bool isActivated;
+        private float expiredTime;
+        public SensorItem(string name, float time)
+        {
+            this.name = name;
+            this.expiredTime = time;
+            this.isActivated = false;
+        }
+
+        public override void ItemInvoke()
+        {
+            if (!isActivated)
+            {
+                this.holder.SendMessage("BeActivated", this.expiredTime);
+                this.isActivated = true;
+                this.CoolDown();
+            }
+            
+        }
+
+        private void CoolDown()
+        {
+            Timer timer = new Timer(args =>
+            {
+                try
+                {
+                    this.isActivated = false;
+                }
+                catch (Exception)
+                {
+                    Debug.Log("激活冷却抛出异常,重新冷却");
+                    Thread.Sleep(100);
+                    this.CoolDown();
+                }
+            }, null, (int)(this.expiredTime * 1000), Timeout.Infinite);
+        }
+    }
+}
---
+++ Assets/Scripts/SenseDoorObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenseDoorObject : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("名称")]
+    private string doorName;
+    [SerializeField]
+    [Tooltip("配对钥匙名称")]
+    private GameObject[] sensors;
+    //是否打开
+    private int activatedCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.activatedCount = 0;
+        foreach (var sensor in sensors)
+        {
+            Debug.Log("与" + sensor.name + "建立链接");
+            sensor.SendMessage("BuildLink", this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.activatedCount >= this.sensors.Length)
+        {
+            this.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+
+    }
+
+    public void BeActivated(float expiredTime)
+    {
+        if (activatedCount >= this.sensors.Length) return;
+
+        Debug.Log("门被激活，计数+1");
+        activatedCount++;
+        this.CountDown(expiredTime);
+    }
+
+    private void CountDown(float expiredTime)
+    {
+        Debug.Log("开始倒计时" + expiredTime.ToString() + "秒");
+        Timer timer = new Timer(args =>
+        {
+            try
+            {
+                this.activatedCount--;
+            }
+            catch (Exception)
+            {
+                Debug.Log("倒计时异常，重新计时");
+                Thread.Sleep(10);
+                this.CountDown(expiredTime);
+            }
+        }, null, (int)(expiredTime * 1000), Timeout.Infinite);
+    }
+}
