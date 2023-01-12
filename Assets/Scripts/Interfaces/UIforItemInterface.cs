26ce2da71de0c823fa5641f0b12b3339f990fc2c
ZorJinklesin <zorjinklesin@gmail.com>
2023-01-12 09:51:28 +0800

    道具与UI互动更新

--- Assets/Scripts/Interfaces/UIforItemInterface.cs
+++ Assets/Scripts/Interfaces/UIforItemInterface.cs
@@ -9,6 +9,24 @@ namespace Interfaces
         //与黑板互动时显示密码UI
         public void ShowBlackBoardContent();
 
+        //与黑板互动时关闭密码UI
+        public void HideBlackBoardContent();
+
+        //查看日记本内容UI
+        public void ShowDiaryContent();
+
+        //收起日记本内容UI
+        public void HideDiaryContent();
+
+        //打开保险箱的密码界面
+        public void ShowPasswordUI();
+
+        //关闭保险箱的密码界面
+        public void HidePasswordUI();
+
+        //输入密码UI， number：0-9的数字字符
+        public void InputPassword(char number);
+
         //道具移除时的UI展示，name：道具名称，type：道具种类
         public void RemoveItem(string name, string type);
 
